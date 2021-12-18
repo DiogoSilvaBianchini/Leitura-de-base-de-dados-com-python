ae71ab6c9e8c6cf37c0e931763627565601bdafc
Diogo do Carmo Silva Bianchini <diogosilvabianchini@gmail.com>
2021-12-18 17:06:58 -0300

    Segundo commit

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 # Importação
+from os import path
 import pandas as pd;
 from twilio.rest import Client
-
+from pathlib import Path
 # Authenticação
 account_sid = "AC4b6814662ce7f1e0788d25f91d08c5de"
-auth_token  = "b25cb99b9479839d2062c462cad6a639"
+auth_token  = "0d4a97a553932759c7f0ce9b7e6d88ad"
 client = Client(account_sid, auth_token)
 
 # Montagem da lógica
 tabelas = ['janeiro','fevereiro','março','abril','maio','junho']
+filePath = './base/'
 cont = 0
-valor = 55000
+valor = 28546
 
 # Montagem da leitura da base de dados em Excel
 for mes in tabelas:
-    tabela_de_vendas = pd.read_excel(f'{mes}.xlsx')
+    tabela_de_vendas = pd.read_excel(Path(f'{filePath}{mes}.xlsx'))
     # Condição que aborda a coluna 'Vendas' da base de dados, e compara com o valor
     if(tabela_de_vendas['Vendas'] > valor).any():
         # Variavel que armazena o vendedor e o quanto ele vendeu
